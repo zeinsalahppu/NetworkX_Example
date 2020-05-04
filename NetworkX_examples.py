8608ff04631f20fc32d48f0bfa5b4333976982d1
zeinsalahppu <zeinsalah@gmail.com>
2020-05-05 01:51:09 +0300

    print some graph information

--- NetworkX_examples.py
+++ NetworkX_examples.py
@@ -16,6 +16,18 @@ G.add_weighted_edges_from([("A", "C", 2), ("B", "C", 5), ("B", "D", 10),
                            ("C", "E", 3), ("D", "F", 11), ("E", "D", 4),])
 
 
+# print some graph info
+print("Nodes of graph: ", end="")
+print(G.nodes())
+print("Edges of graph: ", end="")
+print(G.edges())
+
+print((G.degree("D")))
+print((G.in_degree("D")))
+print((G.out_degree("D")))
+
+
+# draw graph using matplotlib
 pos = nx.spectral_layout(G, 2)
 nx.draw_networkx(G, pos)
 labels = nx.get_edge_attributes(G,'weight')
