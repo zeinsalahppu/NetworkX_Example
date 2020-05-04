05b5a0a653118debe08187f49d481bfbab9cced9
zeinsalahppu <zeinsalah@gmail.com>
2020-05-05 01:49:22 +0300

    draw graph with matplotlob

--- NetworkX_examples.py
+++ NetworkX_examples.py
@@ -5,6 +5,7 @@ Semester: SS 2020
 """
 
 import networkx as nx
+import matplotlib.pyplot as plt
 
 G = nx.DiGraph()
 G.add_node("A")
@@ -14,3 +15,10 @@ G.add_edge("A", "B", weight=4)
 G.add_weighted_edges_from([("A", "C", 2), ("B", "C", 5), ("B", "D", 10),
                            ("C", "E", 3), ("D", "F", 11), ("E", "D", 4),])
 
+
+pos = nx.spectral_layout(G, 2)
+nx.draw_networkx(G, pos)
+labels = nx.get_edge_attributes(G,'weight')
+nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+
+plt.show()
